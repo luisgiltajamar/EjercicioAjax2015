d172dec1a3fa5dbf9c38643a65aa7b41e41d13b8
luis <luis.gil@tajamar365.com>
2015-10-05 10:30:28 +0200

    Final

---
+++ EjercicioAjax/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EjercicioAjax")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EjercicioAjax")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4a1905e2-a74b-4155-96f5-12baff255703")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EjercicioAjax/Scripts/ajax.js
@@ -0,0 +1,80 @@
+﻿var url = "https://alumnoscurso.azure-mobile.net/Tables/Curso";
+
+var cargar = function(datos) {
+    var tabla = "";
+
+    $.each(datos,function(key,elem) {
+
+        tabla += "<tr>";
+        tabla += "<td>" + elem.nombre + "</td>";
+        tabla += "<td>" + elem.duracion + "</td>";
+        tabla += "<td><a href='#' onclick='borrar(\""
+            + elem.id + "\")'>Borrar</a></td>";
+        tabla += "</tr>";
+
+    });
+    $("#cursos").html(tabla);
+};
+var obtener = function() {
+
+    $.getJSON(url, null, cargar);
+};
+var buscar=function() {
+    var u = url;
+    if ($("#txtBus").val() !== "") {
+      //  u += "?$filter=nombre eq '" + $("#txtBus").val() + "'";
+        u += "?$filter=substringof('" + $("#txtBus").val() +
+            "',nombre)";
+    }
+    $.getJSON(u, null, cargar);
+}
+var borrar=function(id) {
+
+    $.ajax({
+        method: "DELETE",
+        url: url + "/" + id,
+        error: function() {
+            alert("Fallo al borrar");
+        },
+        success: obtener
+        
+    });
+}
+var verFormulario=function() {
+    $("#nuevo").show();
+}
+var alta=function() {
+    var data = {
+        nombre: $("#txtNom").val(),
+        duracion: $("#txtDur").val()
+    };
+    $.ajax({
+
+        method: "POST",
+        url: url,
+        data: JSON.stringify(data),
+        dataType: "json",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        success: function() {
+            obtener();
+            $("#nuevo").hide();
+        },
+        error: function() {
+            alert("Error al guardar");
+        }
+    });
+
+}
+
+$(document).ready(function() {
+
+    obtener();
+
+    $("#btnBus").bind("click", buscar);
+    $("#btnNue").click(verFormulario);
+    $("#btnGua").click(alta);
+
+
+});
